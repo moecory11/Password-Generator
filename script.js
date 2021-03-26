d529a444bc0b8798eaac2af14f87c459ec5df167
Cory Moe <moecory11@gamil.com>
2021-03-25 20:53:16 -0700

    added var and prompts

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
+var generateBtn = document.querySelector("#generate");
+  // lowercase array
+var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+// uppercase array
+var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+// special characters array
+var specialcharacters =["!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","]","{","}",":","|",";","<",">",",",".","/","?"]; 
+// numbers array
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+// passwordLength
+var passwordLength = function() {
+  (min >= 8 && max <= 128)
+}
 // Assignment Code
 //DO NOT CHANGE THIS CODE
-var generateBtn = document.querySelector("#generate");
-
 function generatePassword(){
-  //TODO: your code here
+  generateBtn.addEventListener('click', function() {
+  var passwordLength = prompt("How long would you like your password");
+});
+prompt("What character types would you like to include?")
+prompt("Do you want to include lowercase?")
+prompt("Do you want to include uppercase?")
+prompt("Do you want to include numbers?") 
+prompt("Do you want to include special characters?")
+for (let passwordLength = 8; passwordLength < 128; passwordLength++) {
+  const element = [passwordLength];
+  
+}
+/* // create variables
+  // possible characters array
+  // variables for responses to confirms/prompts
+  // create a variable that will hold the final password
+// Display a prompt asking the user for password length
+// TODO: match edges cases - is a number && is >=8 , <=128
+// TODO: ensure that they answered true to at least on confirm
+// display confirms for all different character types (listed in README)
+// check IF the user answered true to any of the confirms, add that array to the possible character array
+​// want to create a loop for as long as passwordLength
+// for each iteration, select a random char out of the possible characters array and add it to the final password variable
+​// return the final password from the generatePassword function
+//TODO: your code here*/
   return "extraSecretPassword"
 }
 
