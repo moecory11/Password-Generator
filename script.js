a923fa01bbddae5838f2b9562c20c2f5ab448559
Cory Moe <moecory11@gamil.com>
2021-03-29 08:39:40 -0700

    updated results

--- script.js
+++ script.js
@@ -1,48 +1,72 @@
 // create variables
 var generateBtn = document.querySelector("#generate");
-  // lowercase array
-var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+// lowercase array
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 // uppercase array
-var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 // special characters array
-var specialcharacters =["!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","]","{","}",":","|",";","<",">",",",".","/","?"]; 
+var specialcharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", ":", "|", ";", "<", ">", ",", ".", "/", "?"];
 // numbers array
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 // passwordLength
-var passwordLength = function() {
+var passwordLength = function () {
   (min >= 8 && max <= 128)
-  // possible characters array
-  var possibleCharacters = [uppercase, lowercase, specialcharacters, numbers];
-}
+};
+
+
+
 // Assignment Code
 //DO NOT CHANGE THIS CODE
-function generatePassword(){
-  generateBtn.addEventListener('click', function() {
-  var passwordLength = prompt("How long would you like your password");
-});
-// Display a prompt asking the user for password length
-prompt("What character types would you like to include?")
-prompt("Do you want to include lowercase?")
-prompt("Do you want to include uppercase?")
-prompt("Do you want to include numbers?") 
-prompt("Do you want to include special characters?")
+function generatePassword() {
+  var passwordLength = parseInt(prompt("How long would you like your password"));
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("try again. Please choose between 8 and 128.")
+    return
+  }
 
+  // possible characters array
+  var possibleCharacters = [];
+
+  // Display a prompt asking the user for password length
+  alert("What character types would you like to include?")
+  if (confirm("Do you want to include lowercase?")) {
+    possibleCharacters = possibleCharacters.concat(lowercase);
+  }
+  if (confirm("Do you want to include uppercase?")) {
+    possibleCharacters = possibleCharacters.concat(uppercase);
+  }
+if (confirm("Do you want to include numbers?")) {
+  possibleCharacters = possibleCharacters.concat(numbers);
+}
+if (confirm("Do you want to include special characters?")) {
+  possibleCharacters = possibleCharacters.concat(specialcharacters);
+}
+console.log(possibleCharacters);
+var results = [];
+for (let i = 0; i < passwordLength; i++) {
+ var item = possibleCharacters[Math.floor(Math.random() * possibleCharacters.length)]; 
+  results.push(item);
+}
+  console.log(results);
   
-/* 
   
-  // variables for responses to confirms/prompts
-  // create a variable that will hold the final password
 
-// TODO: match edges cases - is a number && is >=8 , <=128
-// TODO: ensure that they answered true to at least on confirm
-// display confirms for all different character types (listed in README)
-// check IF the user answered true to any of the confirms, add that array to the possible character array
-​// want to create a loop for as long as passwordLength
-// for each iteration, select a random char out of the possible characters array and add it to the final password variable
-​// return the final password from the generatePassword function
 
-//TODO: your code here*/
-  return "extraSecretPassword"
+  /* 
+    
+    // variables for responses to confirms/prompts
+    // create a variable that will hold the final password
+  
+  // TODO: match edges cases - is a number && is >=8 , <=128
+  // TODO: ensure that they answered true to at least one confirm
+  // display confirms for all different character types (listed in README)
+  // check IF the user answered true to any of the confirms, add that array to the possible character array
+   // want to create a loop for as long as passwordLength
+  // for each iteration, select a random char out of the possible characters array and add it to the final password variable
+   // return the final password from the generatePassword function
+  
+  //TODO: your code here*/
+  return results.join("");
 }
 
 // Write password to the #password input
